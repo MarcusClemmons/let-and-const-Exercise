952c0b2ea7f26b8ef2f82306a320b9085ccbb6b0
Marcus <marcusclemmons1@gmail.com>
2021-10-18 17:22:36 -0400

    files done

---
+++ Exercises.js
@@ -0,0 +1,15 @@
+const PI = 3.14;
+PI = 42;
+
+
+//1) What is the difference between var and let?
+//Var can reassign, redeclare, mutate, and has a functional scope. Let can reassign, cannot redeclare, can mutate, and has a block scope.
+
+//2) What is the difference between var and const?
+//Var can reassign, redeclare, mutate, and has a functional scope. Const cannot reassign, cannot redeclare, it can mutate, and has a block scope.
+
+//3) What is the difference between let and const?
+//Let can reassign, cannot redeclare, can mutate, and has a block scope. Const cannot reassign, cannot redeclare, it can mutate, and has a block scope.
+
+//4) What is hoisting?
+//Hoisting refers to the process whereby the interpreter allocates memory for variable and function declarations prior to execution of the code. Declarations that are made using var are initialized with a default value of undefined. Declarations made using let and const are not initialized as part of hoisting.
